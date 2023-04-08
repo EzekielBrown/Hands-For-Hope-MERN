c8f0f5ce69f9b8fed5a97a2fc4ed926de3b7ac8d
EzekielBrown <zek.bro@hotmail.com>
2023-04-08 19:59:14 +1200

    user/post routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { verifyToken } from './middleware/auth.js';
+import { createPost } from './controllers/posts.js';
 
 // config
 
@@ -35,6 +41,16 @@ const storage = multer.diskStorage({
     },
   });
   const upload = multer({ storage });
+
+// routes with files
+
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+// routes
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // mongoose setup
 
